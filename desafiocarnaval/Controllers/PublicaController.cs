cd8b5b5f4eda56dc89cf4d2e1f25d8f636f81977
Fernando Mota <fmsilva@id.uff.br>
2018-02-05 18:51:41 -0200

    initial commit

---
+++ desafiocarnaval/Controllers/PublicaController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace desafiocarnaval.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Publica")]
+    public class PublicaController : Controller
+    {
+	    IModel model = Program._model;
+		IBasicProperties basicProperties = Program._basicProperties;
+
+	    //public PublicaController(IModel _model, IBasicProperties _basicProperties)
+	    //{
+		   // model = Program._model;
+		   // basicProperties = Program._basicProperties;
+	    //}
+
+        // GET: api/Publica
+        [HttpGet]
+        public void Get()
+        {
+        }
+
+        // GET: api/Publica/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Publica
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+	        byte[] mensagem = Encoding.UTF8.GetBytes(value);
+	        model.BasicPublish("exchangeDesafio",Request.Headers["RoutingKey"],basicProperties, mensagem);
+        }
+        
+        // PUT: api/Publica/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ desafiocarnaval/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace desafiocarnaval
+{
+    public class Program
+    {
+	    public static IConnection _connection;
+	    public static IModel _model;
+	    public static IBasicProperties _basicProperties;
+
+        public static void Main(string[] args)
+        {
+	        ConfiguraRabbit();
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+	    public static void ConfiguraRabbit()
+	    {
+		    ConnectionFactory connectionFactory = new ConnectionFactory();
+		    connectionFactory.HostName = "localhost";
+		    connectionFactory.UserName = "guest";
+		    connectionFactory.Password = "guest";
+ 
+		    _connection = connectionFactory.CreateConnection();
+
+		    _model = _connection.CreateModel();  
+
+		    _model.QueueDeclare("queueDesafio", true, false, false, null);  
+
+		    _model.ExchangeDeclare("exchangeDesafio", ExchangeType.Topic, true);
+
+		    _model.QueueBind("queueDesafio", "exchangeDesafio", "desafio");
+
+		    _basicProperties = _model.CreateBasicProperties();
+		    _basicProperties.SetPersistent(true);
+		
+	
+	    }
+    }
+}
